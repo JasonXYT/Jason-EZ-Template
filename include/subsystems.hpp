99c6d234dd72339d1c5862bbb017f51c36d33979
JasonXYT <xiao.jason2007@gmail.com>
2024-11-02 20:45:25 -0600

    corrected port numbers, added explaination on how to define ports

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -2,10 +2,11 @@
 
 #include "api.h"
 
+// this is where you define your ports on your Vex brain
 
-inline pros::MotorGroup intake({9, 10});  // Negative port will reverse the motor
+inline pros::MotorGroup intake({1, 11});  // Negative port will reverse the motor
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor tilter(16);
+inline pros::Motor tilter(9);
 // inline pros::adi::DigitalIn limit_switch('A');
